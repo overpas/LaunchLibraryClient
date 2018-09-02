4fd99e8c5a7c1250e44aa68146324eb810b732ea
overpas <pckeycalculator@gmail.com>
2018-09-02 13:48:09 +0300

    launch screen improvements

--- app/src/main/java/by/overpass/soraac/Constants.kt
+++ app/src/main/java/by/overpass/soraac/Constants.kt
@@ -1,6 +1,7 @@
 package by.overpass.soraac
 
 const val LAUNCH_ID_KEY = "LAUNCH_ID_KEY"
+const val DEFAULT_MAP_LINK = "http://maps.google.com/maps?q=0+N,0+W"
 
 /**
  * The vital question or what is to be done
--- app/src/main/java/by/overpass/soraac/ui/launch/fragment/LaunchFragment.kt
+++ app/src/main/java/by/overpass/soraac/ui/launch/fragment/LaunchFragment.kt
@@ -2,17 +2,22 @@ package by.overpass.soraac.ui.launch.fragment
 
 
 import android.arch.lifecycle.Observer
+import android.content.Intent
+import android.graphics.Paint
+import android.net.Uri
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import by.overpass.soraac.DEFAULT_MAP_LINK
 import by.overpass.soraac.LAUNCH_ID_KEY
 import by.overpass.soraac.R
 import by.overpass.soraac.databinding.FragmentLaunchBinding
 import by.overpass.soraac.ui.base.fragment.BaseItemFragment
 import by.overpass.soraac.viewmodel.ViewModelFactory
 import by.overpass.soraac.viewmodel.launch.LaunchViewModel
+import kotlinx.android.synthetic.main.fragment_launch.*
 
 class LaunchFragment : BaseItemFragment() {
 
@@ -32,6 +37,20 @@ class LaunchFragment : BaseItemFragment() {
             })
         }
         return binding.root
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        tvLocation.paintFlags = tvLocation.paintFlags or Paint.UNDERLINE_TEXT_FLAG
+        binding.mapOpener = MapOpener(tvLocation)
+    }
+
+    class MapOpener(val view: View) {
+        fun open(mapLink: String?) {
+            view.context?.startActivity(
+                    Intent(Intent.ACTION_VIEW, Uri.parse(mapLink ?: DEFAULT_MAP_LINK))
+            )
+        }
     }
 
 }
